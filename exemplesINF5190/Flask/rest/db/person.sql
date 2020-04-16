7f76267deabd1a605d7295d9ed81e73ee540e17c
Boyan Bechev <boyan.b.2012@feg.bg>
2020-04-16 17:04:19 +0300

    ?final?

--- exemplesINF5190/Flask/rest/db/person.sql
+++
@@ -1,5 +0,0 @@
-create table person (
-  lastname varchar(50),
-  firstname varchar(50),
-  age integer
-);
--- exemplesINF5190/auth/users.sql
+++
@@ -1,6 +0,0 @@
-create table users (
-  id integer primary key,
-  utilisateur varchar(25),
-  salt varchar(32),
-  hash varchar(128)
-);
